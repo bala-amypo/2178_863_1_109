fab687c775a8befac15e660ccd26054ea868d829
bala-amypo <bala@amypo.in>
2025-12-20 17:10:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Credit Card Recommendation API")
+                        .description("Backend API for credit card recommendations")
+                        .version("1.0"));
+    }
+}
