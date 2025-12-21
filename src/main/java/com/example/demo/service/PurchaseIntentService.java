7d4c6cfc9e5b0bd71915dc0bc51299efa9ef0062
bala-amypo <bala@amypo.in>
2025-12-21 06:27:41 +0000

    User commit

--- src/main/java/com/example/demo/service/PurchaseIntentService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.PurchaseIntentRecord;
-import java.util.List;
-
-public interface PurchaseIntentService {
-    PurchaseIntentRecord createIntent(PurchaseIntentRecord intent);
-    List<PurchaseIntentRecord> getAllIntents();
-}
--- src/main/java/com/example/demo/service/RewardRuleService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.RewardRule;
-import java.util.List;
-
-public interface RewardRuleService {
-    RewardRule createRule(RewardRule rule);
-    List<RewardRule> getAllRules();
-}
