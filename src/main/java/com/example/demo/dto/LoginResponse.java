d907738c97d5d7bba9177b4842a2a7fe07493ea3
bala-amypo <bala@amypo.in>
2025-12-26 10:34:05 +0000

    User commit

--- src/main/java/com/example/demo/dto/LoginResponse.java
+++ src/main/java/com/example/demo/dto/LoginResponse.java
@@ -3,10 +3,14 @@ package com.example.demo.dto;
 public class LoginResponse {
     private String token;
 
+    // Constructors
+    public LoginResponse() {}
+
     public LoginResponse(String token) {
         this.token = token;
     }
 
+    // Getter and Setter
     public String getToken() {
         return token;
     }
